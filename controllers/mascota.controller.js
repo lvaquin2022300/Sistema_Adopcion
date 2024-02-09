d70eba50d23f8e9d7229f10aa8eb9d6dd775e2de
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-02-09 11:02:51 -0600

    se modifico controlador de mascotas

--- controllers/mascota.controller.js
+++ controllers/mascota.controller.js
@@ -1,7 +1,78 @@
 const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
-const Usuario = require('../models/mascota');
+const Mascota = require('../models/mascota');
 
-const mascota = async (req, res = response) => {
 
+const mascotasGet = async (req, res = response) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
+
+    const [total, mascotas] = await Promise.all([
+        Mascota.countDocuments(query),
+        Mascota.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        mascotas
+    });
+}
+
+const getMascotaByid = async (req, res) => {
+    const { id } = req.params;
+    const mascota = await Mascota.findOne({ _id: id });
+
+    res.status(200).json({
+        mascota
+    });
+}
+
+
+const mascotasPut = async (req, res) => {
+    const { id } = req.params;
+
+    //Revisar codigo
+    const { _id, nombre, especie, raza, ...resto } = req.body;
+
+    await Mascota.findByIdAndUpdate(id, resto);
+
+    const mascota = await Mascota.findOne({ _id: id });
+
+    res.status(200).json({
+        msg: 'Mascota Actualizado exitosamente',
+        mascota
+    })
+}
+
+
+const mascotasDelete = async (req, res) => {
+    const { id } = req.params;
+    await Mascota.findByIdAndUpdate(id, { estado: false });
+
+    const mascota = await Mascota.findOne({ _id: id });
+
+    res.status(200).json({
+        msg: 'Mascota eliminado exitosamente',
+        mascota
+    });
+}
+
+const mascotasPost = async (req, res) => {
+    const { nombre, edad, especie, raza, servicio } = req.body;
+    const mascota = new Mascota({ nombre, edad, especie, raza, servicio });
+
+    await mascota.save();
+    res.status(200).json({
+        mascota
+    });
+
+    module.exports = {
+        mascotasDelete,
+        mascotasPost,
+        mascotasGet,
+        getMascotaByid,
+        mascotasPut
+    }
 }
