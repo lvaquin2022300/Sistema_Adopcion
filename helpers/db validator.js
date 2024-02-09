f77223caac53d2e558791f906bd34b792e0e110b
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-02-09 08:34:20 -0600

    Se agrego proyecto de mascotas

---
+++ helpers/db validator.js
@@ -0,0 +1,20 @@
+const Usuario = require('../models/usuario');
+
+const existenteEmail = async(correo = '') =>{
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        throw new Error('El email ${correo} ya fue registrado');
+    }
+}
+
+const existeUsuarioById = async (id='') =>{
+    const existeUsuario = await Usuario.findOne({id});
+    if(existeUsuario){
+        throw new Error('El usuario con el ${id} no existe');
+    }
+}
+
+module.exports = {
+    existenteEmail,
+    existeUsuarioById
+}
