0dcf2da304f6459f29b3ccc16a4bca5d99e9ba98
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-02-23 09:30:25 -0600

    Se agregaron modulos

--- controllers/mascota.controller.js
+++
@@ -1,6 +0,0 @@
-const { response } = require('express');
-const Mascota = require('../models/mascota');
-
-const mascotasGet = async (req, res = response) => {
-    
-}
--- models/server.js
+++ models/server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConexion } = require('../db/config');
+
+class Server {
+
+    async conectarDB() {
+        await dbConexion();
+    }
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+    }
+
+}
+
+module.exports = Server;
