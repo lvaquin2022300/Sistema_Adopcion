02ed7a2cd27aa14f6739dbba0d66fa53011c0fd1
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-02-23 09:17:56 -0600

    Se crreo config js

---
+++ db/config.js
@@ -0,0 +1,15 @@
+const { throws } = require('assert');
+const mongoose = require('mongoose');
+
+const dbConexion = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {});
+        console.log("DB conectada");
+    } catch (e) {
+        throw new Error('Error para poder conectar db', e);
+    }
+};
+
+module.exports = {
+    dbConexion
+}
