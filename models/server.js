a9cbce919a26d3237905e02edc075ed94281d6e7
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-02-23 09:37:28 -0600

    Funcionalidad de base de datos

--- models/server.js
+++ models/server.js
@@ -4,17 +4,23 @@ const { dbConexion } = require('../db/config');
 
 class Server {
 
+
+
+    constructor() {
+        this.app = express();
+        this.puerto = process.env.PORT;
+
+        this.conectarDB();
+    }
     async conectarDB() {
         await dbConexion();
     }
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-
-        this.conectarDB();
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en el puerto: ${this.puerto}`);
+        });
     }
-
 }
 
 module.exports = Server;
